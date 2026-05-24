814fa84e51b76b3e219e699281f9fba54daf2f69
prabhurao39 <52779482+prabhurao39@users.noreply.github.com>
2026-05-24 20:25:08 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return isBalanced;
+            maxDepth(root);
+        return  isBalanced;
+    }
+    boolean isBalanced = true;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if (Math.abs(left-right)>1) 
+            isBalanced = false;
+        return Math.max(left , right) +1;
+    }
+}
