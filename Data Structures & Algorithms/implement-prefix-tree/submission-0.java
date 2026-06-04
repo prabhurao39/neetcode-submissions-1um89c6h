585f5a93ff149c35771c38837eb94b100e397f6c
prabhurao39 <52779482+prabhurao39@users.noreply.github.com>
2026-06-04 20:51:25 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,55 @@
+class PrefixTree {
+    static class Node {
+        Character ch;
+        Map<Character, Node> map = new HashMap<>();
+        public Node() {}
+        boolean isEnd = false;
+    }
+    
+    Node root = null;
+    public PrefixTree() {
+        root = new Node();
+    }
+
+    // trie
+    public void insert(String word) {
+        char[] ch =word.toCharArray();
+        Node temp = root;
+        for (char c : ch) {
+            if (!temp.map.containsKey(c)) {
+                Node tail = new Node();
+                temp.map.put(c, tail);
+                temp = tail;
+            } else {
+                temp = temp.map.get(c);
+            }
+        }
+        temp.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Node temp = root;
+        for (char c : word.toCharArray()) {
+            if (!temp.map.containsKey(c)){
+                return false;
+            } else {
+                temp = temp.map.get(c);
+            }
+        }
+
+        return temp.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        Node temp = root;
+        for (char c : prefix.toCharArray()) {
+            if (!temp.map.containsKey(c)){
+                return false;
+            } else {
+                temp = temp.map.get(c);
+            }
+        }
+        
+        return true;
+    }
+}
